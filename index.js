f0957f76193dbf65bd543f8d6a77e5e2e932db27
nishantasarma <nishantasarma@gmail.com>
2019-05-11 19:01:54 +0530

    render

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react"
 import ReactDom from "react-dom"
-
-//JSX
+   
+//JSX Pseudo language javascript rendition of html
 ReactDom.render(<h1>HelloWorld!</h1>,document.getElementById("root"))   
