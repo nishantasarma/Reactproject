9a89ab46bdfbecd680712c108d0e5f071c80226e
nishantasarma <nishantasarma@gmail.com>
2019-05-11 19:06:49 +0530

    functional comp

--- index.js
+++ index.js
@@ -2,4 +2,22 @@ import React from "react"
 import ReactDom from "react-dom"
    
 //JSX Pseudo language javascript rendition of html
-ReactDom.render(<h1>HelloWorld!</h1>,document.getElementById("root"))   
+
+function MyApp() {
+
+	return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+  
+}
+
+
+
+ReactDOM.render(
+  <MyApp />,
+  document.getElementById("root")
+)
