0cad7c4af2965e6f3cb61bacbdda07a4262be5ae
nishantasarma <nishantasarma@gmail.com>
2019-05-13 19:44:58 +0530

    NewAppComp

---
+++ components/App.js
@@ -0,0 +1,27 @@
+import React from "react"
+ 
+export default App
+
+function App(){
+
+	return (
+
+		<div>
+			<nav>
+				<h1> Hello again </h1>
+
+				<ul> 
+					<li>Thing 1</li>
+					<li>Thing 2</li>
+					<li> Thing 3</li>
+
+				</ul>
+
+			</nav>
+			 
+			<main> 
+				<p>This is where most of my contents go. </p> 
+			</main>
+		</div> 
+	)
+}
