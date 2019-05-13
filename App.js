982d35c4f9c954f8146ccba0481a328a459468ef
nishantasarma <nishantasarma@gmail.com>
2019-05-13 21:11:22 +0530

    styling with css

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App(){
+
+	return (
+
+		<div>
+			<header className="navbar">This is the header </h1>
+			<MainContent /> 
+			<Footer />
+		</div> 
+	)
+}
+
+export default App
